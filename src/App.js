26bec64a09576b73aec19670357140185fca184a
mohabahmad <mohabmm666@gmail.com>
2024-10-09 11:38:59 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Footer, Blog, Possibility, Header, WhatGPT3,Features } from './container';
+import { Cta,Brand,Navbar } from './components';
+import './App.css'
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <div className='gradient__bg'>
+            
+            <Navbar/>
+            <Header/>
+            
+        </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <Cta/>
+        
+        <Blog/>
+        <Footer/>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default app
---
+++ src/container/featuers/Featuers.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './featuers.css';
+
+
+const Features = () => {
+  return (
+    <div>
+      Features
+    </div>
+  )
+}
+
+export default Features
