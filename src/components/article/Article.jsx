9631ab3767056de09f89e0787346bde619b581f0
mohabahmad <mohabmm666@gmail.com>
2024-10-14 05:39:22 +0530

    lastupdate

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl,data, title}) => {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container_artical'>
+      <div className='gpt3__blog-container_artical-image'>
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className='gpt3__blog-container_artical-content'>
+        <div>
+          <p>{data}</p>
+          <h3>{title}</h3>
+        </div>
+        <p> Read Full Artical</p>
+      </div>
+      
     </div>
   )
 }
